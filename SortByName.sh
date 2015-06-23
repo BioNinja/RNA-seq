7c7a6deec622bcc2d3760824cef40d1ee26973b4
Zhuoqing Fang <zqfang@login4.(none)>
2015-06-23 15:31:00 -0700

    Test

---
+++ SortByName.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sample=(2D0 2D5 2D5N 2D10 2D10N 2D20R 2D25 8D0 8D5 8D5N 8D10 8D10N 8D20R 8D25)
+for var in ${sample[@]} 
+     do 
+      qsub -pe shared 4 -l h_data=4G,h_rt=8:00:00,highp ~/data/samtools_sorting.sh samtools sort -n ../${var}_Aligned.out.bam  ../${var}_Aligned.out.namesorted
+done
+
--- indexing_samtools.sh
+++ indexing_samtools.sh
@@ -3,6 +3,6 @@ esc8=(8D0 8D5 8D5N 8D10 8D10N 8D20R 8D25)
 sample=(2D0 2D5 2D5N 2D10 2D10N 2D20R 2D25 8D0 8D5 8D5N 8D10 8D10N 8D20R 8D25)
 for var in ${sample[@]} 
      do 
-      qsub -pe shared 4 -l h_data=4G,h_rt=5:00:00,highp ~/data/samtools.sh samtools index ./${var}_Aligned.out.sorted.bam 
+      qsub -pe shared 4 -l h_data=4G,h_rt=5:00:00,highp ~/data/samtools.sh samtools index ../bam_files/${var}_Aligned.sorted.bam 
 done
 
